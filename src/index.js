e3ae75babfaf4394e2ff474660180165bb49dd2b
Vinicius Carvalho <vinicius.carvalho@fraunhofer.pt>
2020-08-20 18:15:22 +0100

    20. Call function inside react.

---
+++ src/index.js
@@ -0,0 +1,27 @@
+// Import the Reac and ReactDOM libraries
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function getButtonText() {
+  return 'Click on me!';
+}
+
+//Create a react component
+const App = function() {
+  
+  return (
+    <div>
+      <label className="label" for="name">Enter name:</label>
+      <input id="name" type="text"/>
+      <button style={{backgroundColor:'blue', color:'white'}} >
+        {getButtonText()}
+      </button>
+    </div>
+  );
+};
+
+// Take the react component and show it on the screen
+ReactDOM.render(
+  <App />,
+  document.querySelector("#root")
+);
