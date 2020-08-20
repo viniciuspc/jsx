2362449359068e91d02866c4bc132c228c00a347
Vinicius Carvalho <vinicius.carvalho@fraunhofer.pt>
2020-08-20 18:23:57 +0100

    20. JSON Object can not be referenced as a childre.

--- src/index.js
+++ src/index.js
@@ -2,19 +2,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-function getButtonText() {
-  return 'Click on me!';
-}
-
 //Create a react component
 const App = function() {
-  
+  const buttonText = {text:  'Click me'}
   return (
     <div>
       <label className="label" for="name">Enter name:</label>
       <input id="name" type="text"/>
       <button style={{backgroundColor:'blue', color:'white'}} >
-        {getButtonText()}
+        {/*We cannot put an JSON Object inside an element as a children it will throw an error.*/}
+        {buttonText.text}
       </button>
     </div>
   );
